da01be61ac15c079074ae366c6c8d8856c476893
Georgios Moutsos <georgios.moutsos@gmail.com>
2022-10-09 12:47:16 +0300

    Added configuration and channels
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/config/RiscoBridgeConfiguration.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/config/RiscoBridgeConfiguration.java
@@ -25,65 +25,41 @@ import org.eclipse.jdt.annotation.Nullable;
 public class RiscoBridgeConfiguration {
 
     // Risco Bridge Thing constants
-    public static final String LISTENING_PORT = "listeningPort";
-    public static final String PANEL_IP = "panelIp";
-    public static final String PANEL_PORT = "panelPort";
-    public static final String PANEL_ID = "panelId";
-    public static final String PANEL_PASSWORD = "panelPassword";
-    public static final String ENCODING = "encoding";
-    public static final String CLOUD_URL = "cloudUrl";
-    public static final String CLOUD_PORT = "cloudPort";
-    public static final String PANEL_CONNECTION_DELAY = "panelConnectionDelay";
-    public static final String SOCKET_MODE = "socketMode";
+    // public static final String PANEL_IP = "hostname";
+    // public static final String PANEL_PORT = "port";
+    // public static final String PANEL_ID = "id";
+    // public static final String PANEL_PASSWORD = "password";
+    // public static final String ENCODING = "encoding";
+    // public static final String PANEL_CONNECTION_DELAY = "connectionDelay";
 
-    private int listeningPort = 33000;
-    private @Nullable String panelIp;
-    private int panelPort = 1000;
-    private int panelId = 1;
-    private boolean guessPasswordAndPanelId = false;
-    private @Nullable String panelPassword;
+    private @Nullable String hostname;
+    private int port = 1000;
+    private int id = 1;
+    private @Nullable String password;
     private String encoding = "utf-8";
-    private String cloudUrl = "www.riscocloud.com";
-    private int cloudPort = 33000;
-    private int panelConnectionDelay = 30000;
+    private int connectionDelay = 0;
 
-    public int getListeningPort() {
-        return listeningPort;
+    public @Nullable String getHostname() {
+        return hostname;
     }
 
-    public @Nullable String getPanelIp() {
-        return panelIp;
+    public int getPort() {
+        return port;
     }
 
-    public int getPanelPort() {
-        return panelPort;
+    public int getId() {
+        return id;
     }
 
-    public int getPanelId() {
-        return panelId;
-    }
-
-    public boolean isGuessPasswordAndPanelId() {
-        return guessPasswordAndPanelId;
-    }
-
-    public @Nullable String getPanelPassword() {
-        return panelPassword;
+    public @Nullable String getPassword() {
+        return password;
     }
 
     public String getEncoding() {
         return encoding;
     }
 
-    public @Nullable String getCloudUrl() {
-        return cloudUrl;
-    }
-
-    public int getCloudPort() {
-        return cloudPort;
-    }
-
-    public int getPanelConnectionDelay() {
-        return panelConnectionDelay;
+    public int getConnectionDelay() {
+        return connectionDelay;
     }
 }
