9d9a0c38cda58047ce60958a377c08a6c4fc0834
Georgios Moutsos <georgios.moutsos@gmail.com>
2022-10-22 23:52:44 +0300

    Initialize panel correctly
    Correct comments/messages

--- bundles/org.openhab.binding.risco/src/test/java/org/openhab/binding/risco/internal/RiscoCommTest.java
+++ bundles/org.openhab.binding.risco/src/test/java/org/openhab/binding/risco/internal/RiscoCommTest.java
@@ -17,10 +17,10 @@ import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
 import org.openhab.binding.risco.internal.RiscoCommunicator.RiscoPanelListener;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 /**
  * Test class for Risco communication.
@@ -29,18 +29,39 @@ import ch.qos.logback.classic.Level;
  */
 @NonNullByDefault
 public class RiscoCommTest implements RiscoPanelListener {
-    private final Logger logger = LoggerFactory.getLogger(RiscoCommTest.class);
+    private final Logger logger = (Logger) LoggerFactory.getLogger(RiscoCommTest.class);
 
     @Test
     public void testCommunication() throws IOException {
-        Logger root = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
-        ((ch.qos.logback.classic.Logger) root).setLevel(Level.DEBUG);
+        Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.DEBUG);
+
+        /*
+         * LoggerContext logCtx = (LoggerContext) LoggerFactory.getILoggerFactory();
+         *
+         * PatternLayoutEncoder logEncoder = new PatternLayoutEncoder();
+         * logEncoder.setContext(logCtx);
+         * logEncoder.setPattern("%-12date{YYYY-MM-dd HH:mm:ss.SSS} %-5level – %msg%n");
+         * logEncoder.start();
+         *
+         * ConsoleAppender logConsoleAppender = new ConsoleAppender();
+         * logConsoleAppender.setContext(logCtx);
+         * logConsoleAppender.setName("console");
+         * logConsoleAppender.setEncoder(logEncoder);
+         * logConsoleAppender.start();
+         *
+         * root.addAppender(logConsoleAppender);
+         */
 
         logger.trace("Hello trace message");
         logger.debug("Hello debug message");
         logger.warn("Hello trace message");
 
-        RiscoCommunicator comm = new RiscoCommunicator("risco", "192.168.1.120", 1000, 1, "UTF-8");
+        logger.trace("Root trace message");
+        logger.debug("Root debug message");
+        logger.warn("Root trace message");
+
+        RiscoCommunicator comm = new RiscoCommunicator("risco", "192.168.1.120", 1000, 1, "UTF-8", null);
         comm.addListener(this);
 
         comm.send("RMT=5678");
@@ -54,12 +75,12 @@ public class RiscoCommTest implements RiscoPanelListener {
         comm.send("ZAREA&*1?");
 
         int mins = 0;
-        int max_mins = 6 * 60;
+        int max_mins = 100 * 6 * 60;
         while (true) {
             try {
                 comm.send("CLOCK");
 
-                Thread.sleep(60 * 1000);
+                Thread.sleep(3 * 1000);
                 mins++;
                 if (mins >= max_mins) {
                     break;
