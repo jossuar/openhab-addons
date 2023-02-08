57ec2bd31d82115a166700257484e827cf38b70a
Georgios Moutsos <georgios.moutsos@gmail.com>
2023-02-08 20:28:00 +0200

    Fix message parsing and add tests
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.risco/src/test/java/org/openhab/binding/risco/internal/RiscoMessageTest.java
+++ bundles/org.openhab.binding.risco/src/test/java/org/openhab/binding/risco/internal/RiscoMessageTest.java
@@ -33,24 +33,24 @@ public class RiscoMessageTest {
     public static final List<Object[]> data() {
         return Arrays.asList(new Object [][]{
             //{"",},
-            { "CLOCK_write", "CLOCK", },
-            { "CUSTLST_write", "CUSTLST", },
-            { "DTYPZ_1-8_read", "DTYPZ", },
-            { "DTYPZ_1-8_write", "DTYPZ", },
-            { "N13", "N", },
-            { "ZLBL_1-8_write", "ZLBL", },
-            { "ZSTT_1-8_read", "ZSTT", },
-            { "ZSTT_19_read", "ZSTT", },
-            { "ZSTT_19_write_on", "ZSTT", },
-            { "ZSTT_19_write_off", "ZSTT", },
-            { "ZTYPE_17-24_write", "ZTYPE", },
+            { "CLOCK_write", "CLOCK", -1, -1},
+            { "CUSTLST_write", "CUSTLST", -1, -1},
+            { "DTYPZ_1-8_read", "DTYPZ", 1, 8},
+            { "DTYPZ_1-8_write", "DTYPZ", 1, 8},
+            { "N13", "N", 13, 13},
+            { "ZLBL_1-8_write", "ZLBL", 1, 8},
+            { "ZSTT_1-8_read", "ZSTT", 1, 8},
+            { "ZSTT_19_read", "ZSTT", 19, 19},
+            { "ZSTT_19_write_on", "ZSTT", 19, 19},
+            { "ZSTT_19_write_off", "ZSTT", 19, 19},
+            { "ZTYPE_17-24_write", "ZTYPE", 17, 24},
         });
     }
     // @formatter:on
 
     @ParameterizedTest
     @MethodSource("data")
-    public void testDecryptEncrypt(String messageName, String commandName) {
+    public void testDecryptEncrypt(String messageName, String commandName, int indexFrom, int indexTo) {
         byte[] bytes = MessageReaderUtil.readRiscoMessage(messageName);
 
         // Decrypt
@@ -60,9 +60,10 @@ public class RiscoMessageTest {
         Integer cmdId = msg.getCommandId();
         String commandStr = msg.getCommand();
         Boolean encrypted = msg.isEncrypted();
-        String name = msg.getCommandName();
 
-        assertEquals(commandName, name);
+        assertEquals(commandName, msg.getCommandName());
+        assertEquals(indexFrom, msg.getIndexFrom());
+        assertEquals(indexTo, msg.getIndexTo());
 
         RiscoMessage msg2 = new RiscoMessage(1, "UTF-8", cmdId, commandStr, encrypted);
         assertArrayEquals(msg.getEncryptedMessage(), msg2.getEncryptedMessage());
