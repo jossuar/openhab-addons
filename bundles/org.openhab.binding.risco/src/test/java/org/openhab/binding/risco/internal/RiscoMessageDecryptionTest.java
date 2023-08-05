62a5c9488f87f7f21412546364f8cdd3633132e7
jossuar <georgios.moutsos@gmail.com>
2023-08-05 13:59:28 +0300

    Correct message value handling and add and additional test
    Signed-off-by: jossuar <georgios.moutsos@gmail.com>

---
+++ bundles/org.openhab.binding.risco/src/test/java/org/openhab/binding/risco/internal/RiscoMessageDecryptionTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.risco.internal;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openhab.binding.risco.internal.message.RiscoMessage;
+import org.openhab.core.util.HexUtils;
+
+/**
+ * Test class for Risco Message.
+ *
+ * @author Georgios Moutsos - Initial contribution
+ */
+@NonNullByDefault
+public class RiscoMessageDecryptionTest {
+    private static final String MESSAGE_EXT = ".txt";
+    private int counter = 1;
+
+    // @formatter:off
+    public static final List<Object> data() {
+        return Arrays.asList(new Object []
+            //{"",},
+            { "PANEL_MESSAGES" });
+    }
+    // @formatter:on
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testMessageHandling(String messageFile) {
+        List<byte[]> messages = readRiscoMessages(messageFile + MESSAGE_EXT);
+
+        for (byte[] m : messages) {
+            checkDecryptEncrypt(m);
+        }
+    }
+
+    private static List<byte[]> readRiscoMessages(String messageFile) {
+        List<byte[]> messages = null;
+
+        try (InputStream is = MessageReaderUtil.class.getResourceAsStream(messageFile);
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr)) {
+
+            messages = br.lines().map((hexString) -> {
+                return HexUtils.hexToBytes(hexString);
+            }).collect(Collectors.toList());
+
+        } catch (IOException e) {
+            throw new AssertionError("IOException reading message data: ", e);
+        }
+
+        return messages;
+    }
+
+    private void checkDecryptEncrypt(byte[] bytes) {
+        // Decrypt
+        RiscoMessage msg = new RiscoMessage(1, "UTF-8", bytes);
+
+        // Get parts
+        Integer cmdId = msg.getCommandId();
+        String commandStr = msg.getFullCommand();
+        Boolean encrypted = msg.isEncrypted();
+
+        PrintStream console = System.out;
+        if (console != null) {
+            console.println(counter++);
+            console.println(msg);
+            console.println("commandId: " + msg.getCommandId());
+            console.println("commandName: " + msg.getCommandName());
+            console.println("hasIndex: " + (msg.hasIndex() ? "true" : "false") + ", hasMultipleIndexes: "
+                    + (msg.hasMultipleIndexes() ? "true" : "false") + ", indexFrom: " + msg.getIndexFrom()
+                    + ", indexTo: " + msg.getIndexTo());
+            console.println("thingIds: " + Arrays.toString(msg.getThingIds()));
+            console.println("properties: " + Arrays.toString(msg.getProperties()));
+            console.println("values: " + Arrays.toString(msg.getCommandValue()));
+            console.println();
+            console.flush();
+        }
+
+        // Encrypt
+        RiscoMessage msg2 = new RiscoMessage(1, "UTF-8", cmdId, commandStr, encrypted);
+        assertArrayEquals(msg.getEncryptedMessage(), msg2.getEncryptedMessage());
+        assertArrayEquals(msg.getDecryptedMessage(), msg2.getDecryptedMessage());
+    }
+}
