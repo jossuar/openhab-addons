c7cfd019513aa748761535a5f89c34f3e6ba0f5c
jossuar <georgios.moutsos@gmail.com>
2023-08-05 22:32:32 +0300

    Correct build warnings
    Signed-off-by: jossuar <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/RiscoMessageZoneStatus.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/RiscoMessageZoneStatus.java
@@ -1,7 +1,22 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.risco.internal.message;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
+/**
+ * @author Georgios Moutsos - Initial contribution
+ */
 @NonNullByDefault
 public class RiscoMessageZoneStatus extends RiscoMessage {
     public RiscoMessageZoneStatus(int commandId, String commandName, String modifier, String[] commandValues,
