40293c6c95635574db7e5c12e506db709cd4aa7f
jossuar <georgios.moutsos@gmail.com>
2023-08-05 21:21:25 +0300

    Refactor message handling [WIP]

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/parser/DTYPZParser.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/parser/DTYPZParser.java
@@ -2,11 +2,10 @@ package org.openhab.binding.risco.internal.message.parser;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.binding.risco.internal.message.KeyValuePair;
+import org.openhab.binding.risco.internal.message.ThingProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,26 +15,27 @@ public class DTYPZParser implements CommandParser {
     private final static Pattern PATTERN = Pattern.compile("^(\\d+),(\\d+),(\\d+),(\\d+),([A-Z]+)$");
 
     @Override
-    public KeyValuePair[] parse(String s) {
-        List<KeyValuePair> data = new ArrayList<KeyValuePair>();
+    public ThingProperty[] parse(String s) {
+        List<ThingProperty> data = new ArrayList<ThingProperty>();
 
-        Matcher m = PATTERN.matcher(s);
-        if (m.matches()) {
-            if ("255".equals(m.group(1))) {
-                data.add(new KeyValuePair("channel",
-                        m.group(5) + " " + "0" + ":" + String.format("%02d", Integer.valueOf(m.group(3))) + ":"
-                                + String.format("%02d", Integer.valueOf(m.group(2)))));
-            } else {
-                data.add(new KeyValuePair("channel",
-                        m.group(5) + " " + (Integer.valueOf(m.group(1)) + 1) + ":"
-                                + String.format("%02d", Integer.valueOf(m.group(3))) + ":"
-                                + String.format("%02d", Integer.valueOf(m.group(2)))));
-            }
-        } else {
-            data.add(new KeyValuePair("channel", "- 0:00:00"));
-            logger.debug("Not expected DTYPZ Value [{}]", s);
-        }
-
-        return data.toArray(new KeyValuePair[0]);
+        /*
+         * Matcher m = PATTERN.matcher(s);
+         * if (m.matches()) {
+         * if ("255".equals(m.group(1))) {
+         * data.add(new ThingProperty("channel",
+         * m.group(5) + " " + "0" + ":" + String.format("%02d", Integer.valueOf(m.group(3))) + ":"
+         * + String.format("%02d", Integer.valueOf(m.group(2)))));
+         * } else {
+         * data.add(new ThingProperty("channel",
+         * m.group(5) + " " + (Integer.valueOf(m.group(1)) + 1) + ":"
+         * + String.format("%02d", Integer.valueOf(m.group(3))) + ":"
+         * + String.format("%02d", Integer.valueOf(m.group(2)))));
+         * }
+         * } else {
+         * data.add(new ThingProperty("channel", "- 0:00:00"));
+         * logger.debug("Not expected DTYPZ Value [{}]", s);
+         * }
+         */
+        return data.toArray(new ThingProperty[0]);
     }
 }
