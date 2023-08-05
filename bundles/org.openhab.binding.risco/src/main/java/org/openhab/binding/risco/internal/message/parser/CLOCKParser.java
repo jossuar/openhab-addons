dfc3ace0fc324e95338774a0c56f1c601e83299f
jossuar <georgios.moutsos@gmail.com>
2023-08-05 21:21:25 +0300

    Refactor message handling [WIP]

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/parser/CLOCKParser.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/message/parser/CLOCKParser.java
@@ -2,11 +2,10 @@ package org.openhab.binding.risco.internal.message.parser;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.binding.risco.internal.message.KeyValuePair;
+import org.openhab.binding.risco.internal.message.ThingProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,20 +15,21 @@ public class CLOCKParser implements CommandParser {
     private final static Pattern PATTERN = Pattern.compile("^(\\d\\d)/(\\d\\d)/(\\d\\d\\d\\d) (\\d\\d):(\\d\\d)$");
 
     @Override
-    public KeyValuePair[] parse(String s) {
-        List<KeyValuePair> data = new ArrayList<KeyValuePair>();
+    public ThingProperty[] parse(String s) {
+        List<ThingProperty> data = new ArrayList<ThingProperty>();
 
-        Matcher m = PATTERN.matcher(s);
-        if (m.matches()) {
-            data.add(new KeyValuePair("day", m.group(1)));
-            data.add(new KeyValuePair("month", m.group(2)));
-            data.add(new KeyValuePair("year", m.group(3)));
-            data.add(new KeyValuePair("hour", m.group(4)));
-            data.add(new KeyValuePair("minute", m.group(5)));
-        } else {
-            logger.debug("Not expected CLOCK Value [{}]", s);
-        }
-
-        return data.toArray(new KeyValuePair[0]);
+        /*
+         * Matcher m = PATTERN.matcher(s);
+         * if (m.matches()) {
+         * data.add(new ThingProperty("day", m.group(1)));
+         * data.add(new ThingProperty("month", m.group(2)));
+         * data.add(new ThingProperty("year", m.group(3)));
+         * data.add(new ThingProperty("hour", m.group(4)));
+         * data.add(new ThingProperty("minute", m.group(5)));
+         * } else {
+         * logger.debug("Not expected CLOCK Value [{}]", s);
+         * }
+         */
+        return data.toArray(new ThingProperty[0]);
     }
 }
