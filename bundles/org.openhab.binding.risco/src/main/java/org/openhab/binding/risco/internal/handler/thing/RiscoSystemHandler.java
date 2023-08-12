07477b69585349cc71ecf3bf49876a941e4dcc08
jossuar <georgios.moutsos@gmail.com>
2023-08-12 17:16:00 +0300

    Enhance zone handler
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/handler/thing/RiscoSystemHandler.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/handler/thing/RiscoSystemHandler.java
@@ -12,14 +12,23 @@
  */
 package org.openhab.binding.risco.internal.handler.thing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.risco.internal.RiscoBindingConstants;
 import org.openhab.binding.risco.internal.handler.RiscoBridgeHandler;
 import org.openhab.binding.risco.internal.handler.RiscoThingHandler;
+import org.openhab.binding.risco.internal.protocol.RiscoProperty;
 import org.openhab.binding.risco.internal.protocol.RiscoThing;
+import org.openhab.binding.risco.internal.protocol.message.PanelConfiguration;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.StringType;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.types.Command;
+import org.openhab.core.types.RefreshType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,8 +48,6 @@ public class RiscoSystemHandler extends RiscoThingHandler {
 
     @Override
     public void initialize() {
-        logger.trace("RiscoSystemHandler initialize");
-
         updateStatus(ThingStatus.OFFLINE);
 
         RiscoBridgeHandler bridgeHandler = getBridgeHandler();
@@ -49,17 +56,56 @@ public class RiscoSystemHandler extends RiscoThingHandler {
         }
 
         bridgeHandler.sendCommand("PNLCNF?");
-    }
-
-    public void updateChannel(ChannelUID channelUID, String data) {
+        logger.trace("RiscoSystemHandler initialized");
     }
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
+        logger.debug("handleCommand(): Command Received - {} {}.", channelUID, command);
+
+        List<String> messages = new ArrayList<String>();
+
+        if (command instanceof RefreshType) {
+            messages.add(PanelConfiguration.getReadCommand());
+        } else {
+            logger.debug("Unknown command {}", command);
+            return;
+        }
+
+        RiscoBridgeHandler bridgeHandler = getBridgeHandler();
+        if (bridgeHandler == null) {
+            return;
+        }
+
+        // Send Zone Status update command
+        for (String m : messages) {
+            bridgeHandler.sendCommand(m);
+        }
     }
 
     @Override
     public void handleEvent(RiscoThing riscoThing) {
+        logger.trace("ZoneHandler received info: {} {}", riscoThing.getRiscoThingType(), riscoThing.getIndex());
+
+        for (RiscoProperty p : riscoThing.getProperties()) {
+            updateChannel(p.getName(), p.getValue());
+        }
+
         updateStatus(ThingStatus.ONLINE);
+    }
+
+    public void updateChannel(String channelID, String data) {
+        logger.trace("Updating system channel: {}, {}", channelID, data);
+
+        if (RiscoBindingConstants.SYSTEM_CHANNEL_NAME.equals(channelID)) {
+            updateState(channelID, new StringType(data));
+
+            logger.trace("  updateChannel: {} = {}", channelID, data);
+        } else {
+            OnOffType onOffType = ("true".equals(data)) ? OnOffType.ON : OnOffType.OFF;
+            updateState(channelID, onOffType);
+
+            logger.trace("  updateChannel: {} = {}", channelID, onOffType);
+        }
     }
 }
