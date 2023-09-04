8db62b807fc311eef5f147dc58d9d6cb99f954d1
jossuar <georgios.moutsos@gmail.com>
2023-09-04 10:41:37 +0300

    Add Partition Arm, Stay, Disarm functionality
    Signed-off-by: jossuar <georgios.moutsos@gmail.com>

---
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/protocol/message/general/PartitionDisarm.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.risco.internal.protocol.message.general;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.risco.internal.protocol.RiscoMessage;
+import org.openhab.binding.risco.internal.protocol.RiscoThing;
+
+/**
+ * @author Georgios Moutsos - Initial contribution
+ */
+@NonNullByDefault
+public class PartitionDisarm extends RiscoMessage {
+    public static final String COMMAND = "DISARM";
+
+    private List<RiscoThing> messageThings = new ArrayList<RiscoThing>();
+
+    public PartitionDisarm(int commandId, String commandName, String modifier, String[] commandValues, int indexFrom,
+            int indexTo, byte[] encryptedMessage, byte[] decryptedMessage) {
+        super(commandId, commandName, modifier, commandValues, indexFrom, indexTo, encryptedMessage, decryptedMessage);
+    }
+
+    @Override
+    public List<RiscoThing> getThings() {
+        return messageThings;
+    }
+
+    public static String getCommand(int partitionNumber) {
+        return String.format(COMMAND + "=%d", partitionNumber);
+    }
+}
