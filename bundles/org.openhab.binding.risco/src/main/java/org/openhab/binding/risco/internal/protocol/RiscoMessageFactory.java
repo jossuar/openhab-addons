da6932ab4f1833181f7335948dc4236a4274fec7
jossuar <georgios.moutsos@gmail.com>
2023-09-04 20:48:26 +0300

    Correct message encryption
    Signed-off-by: jossuar <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/protocol/RiscoMessageFactory.java
+++ bundles/org.openhab.binding.risco/src/main/java/org/openhab/binding/risco/internal/protocol/RiscoMessageFactory.java
@@ -419,12 +419,9 @@ public class RiscoMessageFactory {
             switch (buffer[i]) {
                 case 2:
                 case 3:
+                case 16:
                     outputStream.write(0x10);
                     break;
-                case 16:
-                    if (i != buffer.length - 1) {
-                        outputStream.write(0x10);
-                    }
             }
 
             outputStream.write(buffer[i]);
