5e05139cfa5899210377376c26a85596c17720ea
jossuar <georgios.moutsos@gmail.com>
2024-01-19 23:49:48 +0200

    Update license
    Signed-off-by: jossuar <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.bdrthermea/src/main/java/org/openhab/binding/bdrthermea/internal/handler/BdrThermeaBridgeHandler.java
+++ bundles/org.openhab.binding.bdrthermea/src/main/java/org/openhab/binding/bdrthermea/internal/handler/BdrThermeaBridgeHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2023 Contributors to the openHAB project
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -126,7 +126,6 @@ public class BdrThermeaBridgeHandler extends BaseBridgeHandler {
                 updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, ex.getMessage());
             }
         });
-
     }
 
     private String requestNewToken(String brand, HashMap<String, String> params) {
